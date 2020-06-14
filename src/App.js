167d1c371b50a174a20fb7762bc551a484aaba93
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-14 14:14:52 +0900

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //JSXを使用する場合は下記のReactをimportは必須
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {name: "Tom"}
+    {name: "Alics", age: 3}
   ]
   return (
     <div>
@@ -21,8 +22,10 @@ const User = (props) => {
 return <div>Hi, I am {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.PropTypes = {
+  name: PropTypes.string,
+  //isRequiredを宣言すると属性がない場合もWarningが発生する。
+  age: PropTypes.number.isRequired
 }
 
 export default App;
