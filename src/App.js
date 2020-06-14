2984d59e23e4df547e245940b84afa690f0d8073
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-14 14:52:01 +0900

    state & setState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 //JSXを使用する場合は下記のReactをimportは必須
-import React from 'react';
-import PropTypes from 'prop-types';
+//JSXとはBABELで変換されるトランスファイルでXML形式で記述することができる
+//ComponentをReactからimportするために、{ Component }が必要
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "Alics", age: 3}
-  ]
+//Counterコンポーネントを呼ぶ
+const App = () => (<Counter></Counter>)
+
+//A extends B AはBを継承している
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    //stateを変更する場合にはsetStateを使用する。
+    this.setState({ count: this.state.count + 1})
+  }
+
+  minusButton = () => {
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+  
   return (
-    <div>
-      {
-        profiles.map((profiles,index) => {
-          return <User name = {profiles.name} age = {profiles.age} key={index}/>
-        })
-      }
-    </div>
+    <React.Fragment>
+      <div>count: { this.state.count } </div>
+      <button onClick={ this.handlePlusButton}>+1</button>
+      <button onClick={ this.minusButton}>-1</button>
+    </React.Fragment>
   )
-}
-const User = (props) => {
-return <div>Hi, I am {props.name}, and {props.age} years old.</div>
-}
-
-User.PropTypes = {
-  name: PropTypes.string,
-  //isRequiredを宣言すると属性がない場合もWarningが発生する。
-  age: PropTypes.number.isRequired
-}
+  }
+} 
 
 export default App;
